e719384b140e1deff170c6039b9309f7e3959111
mk124 <mk124a@gmail.com>
2013-07-28 23:03:53 +0800

    添加 AbstractInputDialog 。
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/wl/common/dialog/AbstractInputDialog.java
@@ -0,0 +1,56 @@
+package net.gtaun.wl.common.dialog;
+
+import net.gtaun.shoebill.Shoebill;
+import net.gtaun.shoebill.common.dialog.AbstractDialog;
+import net.gtaun.shoebill.object.Player;
+import net.gtaun.util.event.EventManager;
+
+public class AbstractInputDialog extends net.gtaun.shoebill.common.dialog.AbstractInputDialog
+{
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager)
+	{
+		super(player, shoebill, rootEventManager);
+	}
+
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, AbstractDialog parentDialog)
+	{
+		super(player, shoebill, rootEventManager, parentDialog);
+	}
+
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, boolean passwordMode)
+	{
+		super(player, shoebill, rootEventManager, passwordMode);
+	}
+	
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, boolean passwordMode, AbstractDialog parentDialog)
+	{
+		super(player, shoebill, rootEventManager, passwordMode, parentDialog);
+	}
+
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, String caption, String message)
+	{
+		super(player, shoebill, rootEventManager, caption, message);
+	}
+
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, String caption, String message, AbstractDialog parentDialog)
+	{
+		super(player, shoebill, rootEventManager, caption, message, parentDialog);
+	}
+
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, String caption, String message, boolean passwordMode)
+	{
+		super(player, shoebill, rootEventManager, caption, message, passwordMode);
+	}
+	
+	public AbstractInputDialog(Player player, Shoebill shoebill, EventManager rootEventManager, String caption, String message, boolean passwordMode, AbstractDialog parentDialog)
+	{
+		super(player, shoebill, rootEventManager, caption, message, passwordMode, parentDialog);
+	}
+	
+	@Override
+	protected void onClickCancel()
+	{
+		player.playSound(1084, player.getLocation());
+		showParentDialog();
+	}
+}
