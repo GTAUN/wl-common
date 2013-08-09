fad4b393685ac8a737a3eb1a053fa30544ce7c1c
mk124 <mk124a@gmail.com>
2013-08-09 23:00:36 +0800

    解除封印（无误
    Signed-off-by: mk124 <mk124a@gmail.com>

--- src/main/java/net/gtaun/wl/common/textdraw/TextDrawUtils.java
+++ src/main/java/net/gtaun/wl/common/textdraw/TextDrawUtils.java
@@ -13,6 +13,8 @@
 
 package net.gtaun.wl.common.textdraw;
 
+import org.apache.commons.lang3.StringUtils;
+
 import net.gtaun.shoebill.SampObjectFactory;
 import net.gtaun.shoebill.object.Player;
 import net.gtaun.shoebill.object.PlayerTextdraw;
@@ -25,15 +27,15 @@ public final class TextDrawUtils
 		return textdraw;
 	}
 	
-//	public static PlayerTextdraw createPlayerTextBG(SampObjectFactory f, Player player, float x, float y, float w, float h)
-//	{
-//		int lines = Math.round((h - 5) / 5.0f);
-//		PlayerTextdraw textdraw = f.createPlayerTextdraw(player, x+4, (y-50)/1.075f+50, StringUtils.repeat("~n~", lines));
-//		textdraw.setUseBox(true);
-//		textdraw.setLetterSize(1.0f, 0.5f);
-//		textdraw.setTextSize(x+w-4, h-7);
-//		return textdraw;
-//	}
+	public static PlayerTextdraw createPlayerTextBG(SampObjectFactory f, Player player, float x, float y, float w, float h)
+	{
+		int lines = Math.round((h - 5) / 5.0f);
+		PlayerTextdraw textdraw = f.createPlayerTextdraw(player, x+4, (y-50)/1.075f+50, StringUtils.repeat("~n~", lines));
+		textdraw.setUseBox(true);
+		textdraw.setLetterSize(1.0f, 0.5f);
+		textdraw.setTextSize(x+w-4, h-7);
+		return textdraw;
+	}
 	
 	
 	private TextDrawUtils()
