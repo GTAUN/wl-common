20e4ee25cd4f5b3b502fe6a2cc1266b60fcc81d5
mk124 <mk124a@gmail.com>
2013-09-19 07:13:50 +0800

    Missing.
    Signed-off-by: mk124 <mk124a@gmail.com>

--- src/main/java/net/gtaun/wl/common/dialog/DialogUtils.java
+++ src/main/java/net/gtaun/wl/common/dialog/DialogUtils.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public final class DialogUtils
 {
-	public String rightPad(String str, int size, int tabSize)
+	public static String rightPad(String str, int size, int tabSize)
 	{
 		return str + StringUtils.repeat('\t', (size - str.length()) / tabSize);
 	}
