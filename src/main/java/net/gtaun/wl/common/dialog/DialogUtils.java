cfee7959b5347d4c246e049aebebad3237a5c784
mk124 <mk124a@gmail.com>
2013-09-19 07:12:57 +0800

    Add DialogUtils.
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/wl/common/dialog/DialogUtils.java
@@ -0,0 +1,16 @@
+package net.gtaun.wl.common.dialog;
+
+import org.apache.commons.lang3.StringUtils;
+
+public final class DialogUtils
+{
+	public String rightPad(String str, int size, int tabSize)
+	{
+		return str + StringUtils.repeat('\t', (size - str.length()) / tabSize);
+	}
+	
+	private DialogUtils()
+	{
+		
+	}
+}
