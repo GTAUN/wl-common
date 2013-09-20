559accb3e26cd7b1c9499411f9a0b3cae1f907d2
mk124 <mk124a@gmail.com>
2013-09-21 07:11:27 +0800

    添加 UnitUtils 。
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/wl/common/UnitUtils.java
@@ -0,0 +1,29 @@
+package net.gtaun.wl.common;
+
+public final class UnitUtils
+{
+	public static float kmToMi(float km)
+	{
+		return km * 0.621371192f;
+	}
+	
+	public static double kmToMi(double km)
+	{
+		return km * 0.621371192d;
+	}
+	
+	public static float meterToYard(float m)
+	{
+		return m * 1.0936133f;
+	}
+	
+	public static double meterToYard(double m)
+	{
+		return m * 1.0936133d;
+	}
+	
+	private UnitUtils()
+	{
+		
+	}
+}
